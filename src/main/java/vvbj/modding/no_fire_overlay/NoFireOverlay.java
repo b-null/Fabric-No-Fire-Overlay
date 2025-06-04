2a8b3ae03faee43bc672ffe9e631537a22acba0e
Brian <bvergara.vj@gmail.com>
2025-06-04 19:09:29 -0400

    Current

---
+++ src/main/java/vvbj/modding/no_fire_overlay/NoFireOverlay.java
@@ -0,0 +1,16 @@
+package vvbj.modding.no_fire_overlay;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NoFireOverlay implements ModInitializer {
+	public static final String MOD_ID = "no-fire-overlay";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("No Fire overlay loaded. Say bye bye to that annoying fire!");
+	}
+}
