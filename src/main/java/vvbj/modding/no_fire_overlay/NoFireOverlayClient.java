6e5c9a7fec5466685b575706224f82664a0af82b
Brian <bvergara.vj@gmail.com>
2025-07-27 23:18:29 -0400

    Replaced deprecated HudRenderCallback event with HudLayerRegistrationCallback event

--- src/main/java/vvbj/modding/no_fire_overlay/NoFireOverlayClient.java
+++ src/main/java/vvbj/modding/no_fire_overlay/NoFireOverlayClient.java
@@ -2,7 +2,8 @@ package vvbj.modding.no_fire_overlay;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.util.Identifier;
@@ -10,22 +11,25 @@ import net.minecraft.util.Identifier;
 public class NoFireOverlayClient implements ClientModInitializer {
 
     private static final Identifier FIRE_TEXTURE = Identifier.of(NoFireOverlay.MOD_ID, "fire.png");
+    private static final Identifier OVERLAY_ID = Identifier.of(NoFireOverlay.MOD_ID, "fire_icon");
 
     private static int counter = 0;
 
     @Override
     public void onInitializeClient() {
-        HudRenderCallback.EVENT.register((drawContext, delta) -> {
-            MinecraftClient client = MinecraftClient.getInstance();
-            if(client.player != null && client.player.isOnFire() && !client.options.hudHidden && client.options.getPerspective().isFirstPerson()) {
-                int centerX = drawContext.getScaledWindowWidth() / 2;
-                int centerY = drawContext.getScaledWindowHeight() / 2;
+        HudLayerRegistrationCallback.EVENT.register(drawerWrapper -> {
+            drawerWrapper.addLayer(IdentifiedLayer.of(OVERLAY_ID, (context, tickCounter) -> {
+                MinecraftClient client = MinecraftClient.getInstance();
+                if(client.player != null && client.player.isOnFire() && !client.options.hudHidden && client.options.getPerspective().isFirstPerson()) {
+                    int centerX = context.getScaledWindowWidth() / 2;
+                    int centerY = context.getScaledWindowHeight() / 2;
 
-                int u = 0;
-                int v = 16 * counter;
+                    int u = 0;
+                    int v = 16 * counter;
 
-                drawContext.drawTexture(RenderLayer::getGuiTextured, FIRE_TEXTURE, centerX + 8, centerY - 2, u, v, 4, 4, 16, 16, 16, 512);
-            }
+                    context.drawTexture(RenderLayer::getGuiTextured, FIRE_TEXTURE, centerX + 8, centerY - 2, u, v, 4, 4, 16, 16, 16, 512);
+                }
+            }));
         });
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
