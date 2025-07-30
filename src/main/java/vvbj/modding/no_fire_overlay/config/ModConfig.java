eca50bdfd3e6cb49ff4f6c919450cff2fa25edd5
Brian <bvergara.vj@gmail.com>
2025-07-30 03:49:36 -0400

    Got rid of cloth config and mod menu

--- src/main/java/vvbj/modding/no_fire_overlay/config/ModConfig.java
+++ src/main/java/vvbj/modding/no_fire_overlay/config/ModConfig.java
@@ -1,13 +1,10 @@
 package vvbj.modding.no_fire_overlay.config;
 
-import me.shedaniel.autoconfig.ConfigData;
-import me.shedaniel.autoconfig.annotation.Config;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 @Environment(EnvType.CLIENT)
-@Config(name = "no-fire-overlay")
-public class ModConfig implements ConfigData {
+public class ModConfig {
     public boolean enabled = true;
 
     public OverlayMode mode = OverlayMode.HIDE;
