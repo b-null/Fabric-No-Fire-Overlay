17c36d60e2cacaa59514b2fb9c6e0578e03d3749
Brian <bvergara.vj@gmail.com>
2025-07-30 02:22:36 -0400

    InGameHudMixin renamed to InGameOverlayRendererMixin

--- src/main/java/vvbj/modding/no_fire_overlay/mixin/InGameOverlayRendererMixin.java
+++ src/main/java/vvbj/modding/no_fire_overlay/mixin/InGameOverlayRendererMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameOverlayRenderer.class)
-public abstract class InGameHudMixin {
+public abstract class InGameOverlayRendererMixin {
 
     @Inject(method = "renderFireOverlay", at = @At("HEAD"), cancellable = true)
     private static void cancelFireOverlay(MinecraftClient client, MatrixStack matrices, CallbackInfo ci){
