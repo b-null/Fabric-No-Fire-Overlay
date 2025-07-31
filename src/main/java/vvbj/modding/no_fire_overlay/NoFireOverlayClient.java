5b18b9e4e4e011e95eb47401d3b4f714f7a85eb6
Brian <bvergara.vj@gmail.com>
2025-07-30 21:13:36 -0400

    Added config

--- src/main/java/vvbj/modding/no_fire_overlay/NoFireOverlayClient.java
+++ src/main/java/vvbj/modding/no_fire_overlay/NoFireOverlayClient.java
@@ -2,12 +2,16 @@ package vvbj.modding.no_fire_overlay;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
+import vvbj.modding.no_fire_overlay.config.ConfigHandler;
+import vvbj.modding.no_fire_overlay.config.ModConfig;
 
 public class NoFireOverlayClient implements ClientModInitializer {
 
+    private ModConfig config;
     private static final Identifier FIRE_TEXTURE = Identifier.of(NoFireOverlay.MOD_ID, "fire.png");
 
     private static int counter = 0;
@@ -15,15 +19,27 @@ public class NoFireOverlayClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HudRenderCallback.EVENT.register((drawContext, delta) -> {
+            if(config == null)
+                config = ConfigHandler.config;
+            if(!config.enabled) return;
+
             MinecraftClient client = MinecraftClient.getInstance();
-            if(client.player != null && client.player.isOnFire() && !client.options.hudHidden && client.options.getPerspective().isFirstPerson()) {
+            if(config.mode == ModConfig.OverlayMode.HIDE && config.showCrosshairFireIcon && client.player != null && client.player.isOnFire() && !client.options.hudHidden && client.options.getPerspective().isFirstPerson()) {
                 int centerX = drawContext.getScaledWindowWidth() / 2;
                 int centerY = drawContext.getScaledWindowHeight() / 2;
+
+                float offset = (4 * config.fireIconSize - 4) / 2;
+
+                drawContext.getMatrices().push();
+                drawContext.getMatrices().translate(centerX + 8 + (int)config.fireIconSize - offset,centerY - 2 - offset,0);
+                drawContext.getMatrices().scale(config.fireIconSize, config.fireIconSize, 1);
 
                 float u = 0;
                 float v = 16 * counter;
 
-                drawContext.drawTexture(FIRE_TEXTURE, centerX + 8, centerY - 2, 4, 4, u, v, 16, 16, 16, 512);
+                drawContext.drawTexture(FIRE_TEXTURE, 0, 0, 4, 4, u, v, 16, 16, 16, 512);
+
+                drawContext.getMatrices().pop();
             }
         });
 
@@ -36,6 +52,10 @@ public class NoFireOverlayClient implements ClientModInitializer {
                 }else
                     counter = 0;
             }
+        });
+
+        ClientPlayConnectionEvents.JOIN.register((clientPlayNetworkHandler, packetSender, minecraftClient) -> {
+            ConfigHandler.loadConfig(); // Refresh
         });
     }
 }
