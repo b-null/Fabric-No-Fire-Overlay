079e0c6d95eddcb6a4b1f8f24fa63070c0d3ec17
Brian <bvergara.vj@gmail.com>
2025-07-30 21:57:02 -0400

    Added config

--- src/main/java/vvbj/modding/no_fire_overlay/NoFireOverlayClient.java
+++ src/main/java/vvbj/modding/no_fire_overlay/NoFireOverlayClient.java
@@ -2,32 +2,45 @@ package vvbj.modding.no_fire_overlay;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.util.Identifier;
+import vvbj.modding.no_fire_overlay.config.ConfigHandler;
+import vvbj.modding.no_fire_overlay.config.ModConfig;
 
 public class NoFireOverlayClient implements ClientModInitializer {
 
     private static final Identifier FIRE_TEXTURE = Identifier.of(NoFireOverlay.MOD_ID, "fire.png");
     private static final Identifier OVERLAY_ID = Identifier.of(NoFireOverlay.MOD_ID, "fire_icon");
 
+    private ModConfig config;
     private static int counter = 0;
 
     @Override
     public void onInitializeClient() {
-
-
         HudElementRegistry.addFirst(OVERLAY_ID, (context, tickCounter) -> {
+            if(config == null)
+                config = ConfigHandler.config;
+            if(!config.enabled) return;
+
             MinecraftClient client = MinecraftClient.getInstance();
-            if(client.player != null && client.player.isOnFire() && !client.options.hudHidden && client.options.getPerspective().isFirstPerson()) {
+            if(config.mode == ModConfig.OverlayMode.HIDE && config.showCrosshairFireIcon && client.player != null && client.player.isOnFire() && !client.options.hudHidden && client.options.getPerspective().isFirstPerson()) {
                 int centerX = context.getScaledWindowWidth() / 2;
                 int centerY = context.getScaledWindowHeight() / 2;
+
+                float offset = (4 * config.fireIconSize - 4) / 2;
+
+                context.getMatrices().pushMatrix();
+                context.getMatrices().translate(centerX + 8 + (int)config.fireIconSize - offset,centerY - 2 - offset);
+                context.getMatrices().scale(config.fireIconSize, config.fireIconSize);
 
                 int u = 0;
                 int v = 16 * counter;
 
-                context.drawTexture(RenderPipelines.GUI_TEXTURED, FIRE_TEXTURE, centerX + 8, centerY - 2, u, v, 4, 4, 16, 16, 16, 512);
+                context.drawTexture(RenderPipelines.GUI_TEXTURED, FIRE_TEXTURE, 0, 0, u, v, 4, 4, 16, 16, 16, 512);
+                context.getMatrices().popMatrix();
             }
         });
 
@@ -40,6 +53,10 @@ public class NoFireOverlayClient implements ClientModInitializer {
                 }else
                     counter = 0;
             }
+        });
+
+        ClientPlayConnectionEvents.JOIN.register((clientPlayNetworkHandler, packetSender, minecraftClient) -> {
+            ConfigHandler.loadConfig(); // Refresh
         });
     }
 }
---
+++ src/main/java/vvbj/modding/no_fire_overlay/config/ConfigHandler.java
@@ -0,0 +1,51 @@
+package vvbj.modding.no_fire_overlay.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
+import vvbj.modding.no_fire_overlay.NoFireOverlay;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Environment(EnvType.CLIENT)
+public class ConfigHandler {
+
+    private static final Gson GSON = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
+    private static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve("no-fire-overlay.json");
+
+    public static ModConfig config = new ModConfig();
+
+    public static void loadConfig(){
+        try {
+            if (Files.exists(CONFIG_PATH)) {
+                Reader reader = Files.newBufferedReader(CONFIG_PATH);
+                JsonObject json = JsonParser.parseReader(reader).getAsJsonObject();
+                config.loadFromJson(json);
+                reader.close();
+            } else {
+                config.reset();
+                saveConfig(); // Write default
+            }
+        } catch (IOException e) {
+            NoFireOverlay.LOGGER.error("Error loading config from path: {}. Attempting to re-create it", e.getMessage());
+            config.reset();
+            saveConfig();
+        }
+    }
+
+    public static void saveConfig(){
+        try (Writer writer = Files.newBufferedWriter(CONFIG_PATH)) {
+            GSON.toJson(config.toJson(), writer);
+        } catch (IOException e) {
+            NoFireOverlay.LOGGER.error("Error saving config to path: {}", e.getMessage());
+        }
+    }
+}
